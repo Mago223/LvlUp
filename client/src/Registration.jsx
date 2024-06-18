4dd90ad9e5ed18e10e86557b7eb997c72c9e4ab4
Marcos Sanabria <sanabriam007@gmail.com>
2024-06-18 16:59:16 -0500

    Form registration component base

---
+++ client/src/Registration.jsx
@@ -0,0 +1,11 @@
+function Registration() {
+    return (
+        <div className="registration">
+            <h1>Ready to LvlUp?</h1>
+            <form method="post" action=""> {/*FIXME: add API endpoint to action*/}
+            </form>
+        </div>
+    );
+}
+
+export default Registration
