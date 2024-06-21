182fac255b892b85e052bd5f6dca48da93a1c7b6
achaney4 <achan014@odu.edu>
2024-06-21 00:17:49 -0400

    added tailwind. added background, title, and instructions.

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,20 @@
 function Login() {
     return (
-        <div className='login'>
-
+        <div className="login">
+            <body className="flex justify-center items-center min-h-screen">
+                <div className="bg-[#1F2025] text-[#181028] p-8 space-y-4 shadow-2xl rounded-xl max-w-full">
+                    <h1 id='logTitle'>Log In</h1>
+                    <p>Welcome back! Please enter your login credentials.</p>
+                </div>
+            </body>
+                
         </div>
+          
 
     );
 
 }
+
+
 
 export default Login
