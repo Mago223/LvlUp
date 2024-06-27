1da4a644188f6063ee280979c96d56a0383fe1ea
achaney4 <achan014@odu.edu>
2024-06-26 21:42:19 -0400

    removed logout and deactivate from router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Registration from "./pages/Registration";
 import Login from "./pages/Login";
-import Logout from "./pages/Logout";
-import Deactivate from "./pages/Deactivate";
+
 
 function App() {
   return (
@@ -11,8 +10,6 @@ function App() {
         <Routes>
           <Route path="/register" element={<Registration />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="/deactivate" element={<Deactivate />} />
         </Routes>
       </BrowserRouter>
     </div>
