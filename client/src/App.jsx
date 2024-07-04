82ead0a86152f584b893e1e5ee17e7279fd2195b
achaney4 <achan014@odu.edu>
2024-07-03 21:35:40 -0400

    removed extra login page link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Registration from "./pages/Registration/Registration";
 import Login from "./pages/Login";
 
+
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/register" element={<Registration />} />
-          <Route path="/login" element={<Login />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
