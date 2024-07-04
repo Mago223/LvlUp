680c10b039ac3880644a22498eae7c8430687b43
Marcos Sanabria <sanabriam007@gmail.com>
2024-07-04 18:01:26 -0500

    removed duplicate route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,6 @@ function App() {
         <Routes>
           <Route path="/register" element={<Registration />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
