cf000bad8206d70a939858d26ff3d29278f59537
modusami <128193218+modusami@users.noreply.github.com>
2024-07-05 12:46:32 -0400

    Replace old schema file

--- server/database/schema.sql
+++
@@ -1,6 +0,0 @@
-/**
- * This file can be used to represent mock schema and tables to provide an overall general structure of the data that will be stored in the backend and also used in the frontend
- * Michael-Andre Odusami
- */
-
-
