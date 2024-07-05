5f77495a5970dab9607c554fb78d74ea543c67b5
modusami <128193218+modusami@users.noreply.github.com>
2024-07-05 12:09:47 -0400

    Create Schema File

---
+++ server/database/schema.sql
@@ -0,0 +1,6 @@
+/**
+ * This file can be used to represent mock schema and tables to provide an overall general structure of the data that will be stored in the backend and also used in the frontend
+ * Michael-Andre Odusami
+ */
+
+
