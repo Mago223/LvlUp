e1b0fbb673ec68c937602f34d9733cbc236d1673
modusami <128193218+modusami@users.noreply.github.com>
2024-07-05 12:36:17 -0400

    Update

--- server/database/create_tables.sql
+++ server/database/create_tables.sql
@@ -7,7 +7,11 @@
 CREATE TABLE "user"(
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
+    full_name VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
+
+INSERT INTO TABLE user (username, fullname, email, password)
+VALUES ('root', 'user test', 'test', 'pass')
