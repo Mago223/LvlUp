b6561527af08001312cb6aaa066ece92c67ec198
modusami <128193218+modusami@users.noreply.github.com>
2024-07-05 15:25:00 -0400

    Add Cors

--- server/index.js
+++ server/index.js
@@ -2,24 +2,40 @@ const express = require("express");
 const app = express();
 const db = require("./db");
 
-//Setting up the db
-app.get('/some-route', async (req, res) => {
-  try {
-    const result = await db.query('SELECT * FROM your_table');
-    console.log('Query result:', result.rows);
-    res.json(result.rows)
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({
-      error: 'Database query failed'
-    });
-  }
-    //res.send('Test')
+// require cors allowing the frontend to make calls to the backend
+const cors = require("cors");
+var corsOptions = {
+	origin: "http://localhost:8081", // allow request from this origin
+};
+
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// additonal non db route
+app.get("/", (req, res) => {
+	res.json({ message: "Welcome to the application Fitness!" });
 });
 
-const PORT = process.env.PORT || 3000;
+//Setting up the db
+app.get("/some-route", async (req, res) => {
+	try {
+		const result = await db.query("SELECT * FROM your_table");
+		console.log("Query result:", result.rows);
+		res.json(result.rows);
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({
+			error: "Database query failed",
+		});
+	}
+	//res.send('Test')
+});
 
+const PORT = process.env.PORT || 3000; // server runs on port 3000 by default
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
+	console.log(`Server is running on port ${PORT}`);
 });
