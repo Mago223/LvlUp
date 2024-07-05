6f542018d570561c57fe5d8b054f30979b6928cf
modusami <128193218+modusami@users.noreply.github.com>
2024-07-05 17:26:11 -0400

    User Auth Working Step 1

--- server/index.js
+++ server/index.js
@@ -1,38 +1,51 @@
 const express = require("express");
+const sequelize = require("sequelize");
+const dotenv = require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const db = require("./model");
+const userRoutes = require("./features/auth/routes/userRoutes");
 const app = express();
-const db = require("./db");
 
 // require cors allowing the frontend to make calls to the backend
-const cors = require("cors");
-var corsOptions = {
-	origin: "http://localhost:8081", // allow request from this origin
-};
+// const cors = require("cors");
+// var corsOptions = {
+// 	origin: "http://localhost:8081", // allow request from this origin
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 // parse requests of content-type - application/json
+
 app.use(express.json());
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+//synchronizing the database and forcing it to false so we dont lose data
+db.sequelize.sync({ force: true }).then(() => {
+	console.log("db has been re sync");
+});
+
+//routes for the user API
+app.use("/api/users", userRoutes);
 
 // additonal non db route
 app.get("/", (req, res) => {
 	res.json({ message: "Welcome to the application Fitness!" });
 });
 
-//Setting up the db
-app.get("/some-route", async (req, res) => {
-	try {
-		const result = await db.query("SELECT * FROM your_table");
-		console.log("Query result:", result.rows);
-		res.json(result.rows);
-	} catch (err) {
-		console.error(err);
-		res.status(500).json({
-			error: "Database query failed",
-		});
-	}
-	//res.send('Test')
-});
+// //Setting up the db
+// app.get("/some-route", async (req, res) => {
+// 	try {
+// 		const result = await db.query("SELECT * FROM your_table");
+// 		console.log("Query result:", result.rows);
+// 		res.json(result.rows);
+// 	} catch (err) {
+// 		console.error(err);
+// 		res.status(500).json({
+// 			error: "Database query failed",
+// 		});
+// 	}
+// 	//res.send('Test')
+// });
 
 const PORT = process.env.PORT || 3000; // server runs on port 3000 by default
 
