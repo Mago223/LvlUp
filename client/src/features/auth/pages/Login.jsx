0082d625cfd19785bdffa25929322bb0bd641f06
Marcos Sanabria <sanabriam007@gmail.com>
2024-07-06 15:53:34 -0500

    fixed right borders of login form

--- client/src/features/auth/pages/Login.jsx
+++ client/src/features/auth/pages/Login.jsx
@@ -5,7 +5,7 @@ function Login() {
   return (
     <div className="login">
       <body className="flex justify-center items-center min-h-screen">
-        <div className="bg-[#1F2025] text-[#181028] p-8 space-y-6 shadow-2xl rounded-xl max-w-full">
+        <div className="bg-[#1F2025] text-[#181028] p-8 space-y-6 shadow-2xl rounded-xl rounded-r-none max-w-full">
           <h1 id="logTitle"> LvlUp </h1> {/* logo */}
           <p>Welcome back! Please enter your login credentials.</p>{" "}
           {/* Instructions */}
