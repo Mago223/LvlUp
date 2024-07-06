de71a86ee93fbb8bf1acf484fe09f6d144b62798
Marcos Sanabria <sanabriam007@gmail.com>
2024-07-06 16:16:41 -0500

    made picture hidden when screen is small

--- client/src/features/auth/pages/Login.jsx
+++ client/src/features/auth/pages/Login.jsx
@@ -5,7 +5,10 @@ function Login() {
   return (
     <div className="login">
       <body className="flex justify-center items-center min-h-screen">
-        <div className="bg-[#1F2025] text-[#181028] p-8 space-y-6 shadow-2xl rounded-xl rounded-r-none max-w-full">
+        <div
+          className="bg-[#1F2025] text-[#181028] p-8 space-y-6 shadow-2xl rounded-xl rounded-r-none max-w-full"
+          id="loginBody"
+        >
           <h1 id="logTitle"> LvlUp </h1> {/* logo */}
           <p>Welcome back! Please enter your login credentials.</p>{" "}
           {/* Instructions */}
@@ -89,7 +92,7 @@ function Login() {
         <div>
           <img
             id="pic"
-            className="rounded-xl rounded-l-none shadow-2xl max-w-full"
+            className="rounded-xl rounded-l-none shadow-2xl max-w-full hidden md:block"
             src={loginPic}
           />{" "}
           {/**Picture */}
