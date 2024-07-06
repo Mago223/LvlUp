a7ef399fe70b7cc71123314dd4babc66ee682ae0
modusami <128193218+modusami@users.noreply.github.com>
2024-07-06 13:39:11 -0400

    Update imports

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
-const sequelize = require("sequelize");
-const dotenv = require("dotenv").config();
 const cookieParser = require("cookie-parser");
-const db = require("./model");
+const db = require("./db.config");
 const userRoutes = require("./features/auth/routes/userRoutes");
 const app = express();
 
@@ -31,21 +29,6 @@ app.use("/api/users", userRoutes);
 app.get("/", (req, res) => {
 	res.json({ message: "Welcome to the application Fitness!" });
 });
-
-// //Setting up the db
-// app.get("/some-route", async (req, res) => {
-// 	try {
-// 		const result = await db.query("SELECT * FROM your_table");
-// 		console.log("Query result:", result.rows);
-// 		res.json(result.rows);
-// 	} catch (err) {
-// 		console.error(err);
-// 		res.status(500).json({
-// 			error: "Database query failed",
-// 		});
-// 	}
-// 	//res.send('Test')
-// });
 
 const PORT = process.env.PORT || 3000; // server runs on port 3000 by default
 
