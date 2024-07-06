e98f30f5fc90d0819f234375c36359760fc9ea77
Marcos Sanabria <sanabriam007@gmail.com>
2024-07-05 20:46:20 -0500

    removed misplaced navbar route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login, Registration, Welcome } from "./features/auth/index";
-import MainNavbar from "./features/home/components/MainNavbar";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
           <Route path="/register" element={<Registration />} />
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Welcome />} /> {/**Home page */}
-          <Route path="/navbar" element={<MainNavbar />} />
         </Routes>
       </BrowserRouter>
     </div>
