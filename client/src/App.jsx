979ae999c60bc90cb8824adcb57ce7dd9947c3f7
modusami <128193218+modusami@users.noreply.github.com>
2024-07-07 16:43:32 -0400

    Add index.js file to the root dir of home
    Each feature should have an index.js file.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login, Registration, Welcome } from "./features/auth/index";
-import Home from "./features/home/pages/Home";
+import { Home } from "./features/home";
 
 function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/register" element={<Registration />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Welcome />} /> {/**welcome page */}
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/register" element={<Registration />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/" element={<Welcome />} /> {/**welcome page */}
+					<Route path="/home" element={<Home />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
