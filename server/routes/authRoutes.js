2cbea898d5276f58f248299f4fe4f1a0361d6098
Marcos Sanabria <sanabriam007@gmail.com>
2024-07-08 16:44:10 -0500

    added API for frontend token verification

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,8 @@
+const router = express.Router();
+const auth = require("../middleware/authMiddleware");
+
+router.get("/check", auth.authenticateToken, (req, res) => {
+  res.status(200).send("Token is valid");
+});
+
+module.exports = router;
