e3169651f716412f8c629c9a85adefa228ae52a4
modusami <128193218+modusami@users.noreply.github.com>
2024-07-09 08:34:34 -0400

    Add an optional testing route (to be deleted)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,24 +4,26 @@ import { Welcome } from "./features/welcome";
 import { Home } from "./features/home";
 import PrivateRoutes from "./components/PrivateRoutes";
 import { AuthProvider } from "./utils/AuthContext";
+import TestRoute from "./features/testRoute/TestRoute";
 
 function App() {
-  return (
-    <div>
-      <Router>
-        <AuthProvider>
-          <Routes>
-            <Route path="/register" element={<Registration />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/" element={<Welcome />} /> {/**welcome page */}
-            <Route element={<PrivateRoutes />}>
-              <Route path="/home" element={<Home />} />
-            </Route>
-          </Routes>
-        </AuthProvider>
-      </Router>
-    </div>
-  );
+	return (
+		<div>
+			<Router>
+				<AuthProvider>
+					<Routes>
+						<Route path="/register" element={<Registration />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/" element={<Welcome />} /> {/**welcome page */}
+						<Route element={<PrivateRoutes />}>
+							<Route path="/home" element={<Home />} />
+							<Route path="/test" element={<TestRoute />} />
+						</Route>
+					</Routes>
+				</AuthProvider>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
