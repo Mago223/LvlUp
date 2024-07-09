5ee29c2f39ecda0f44892b090f5b147dad873eb4
achaney4 <achan014@odu.edu>
2024-07-08 21:45:38 -0400

    removed old code

--- client/src/features/welcome/components/WelcomeNav.jsx
+++ client/src/features/welcome/components/WelcomeNav.jsx
@@ -46,43 +46,6 @@ import {
 function WelcomeNav() {
 	return (
 
-		/* <nav className="bg-[#1F2025] flex justify-between items-center h-20 max-w-8xl px-4 font-bold sm:flex  sm:item-center">
-			
-			<h1 id="logo">LvlUp</h1>
-
-			
-			<div className="sm:flex pt-1">
-				<a
-					href="#"
-					className="text-[#DC2F02] p-3 t m-2 cursor-pointer duration-300 text-lg"
-				>
-					{" "}
-					Meal Plans{" "}
-				</a>
-				<a
-					href="#"
-					className="text-[#E85D04] p-3 t m-2 cursor-pointer duration-300 text-lg"
-				>
-					{" "}
-					Workouts{" "}
-				</a>
-				<a
-					href="#"
-					className="text-[#F48C06] p-3 t m-2 cursor-pointer duration-300 text-lg"
-				>
-					{" "}
-					About Us{" "}
-				</a>
-				<a
-					href="/login"
-					className="text-[#ffba08] p-3 t m-2 cursor-pointer duration-300 text-lg"
-				>
-					{" "}
-					Log In{" "}
-				</a>
-			</div>
-		</nav> */
-
 	<Disclosure as="nav" className="bg-dark-grey shadow-md" id="nav">
       <div className="mx-auto max-w-7xl">
         <div className="relative flex h-16 items-center justify-between">
