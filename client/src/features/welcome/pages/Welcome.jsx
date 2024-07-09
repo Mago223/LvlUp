0a32620d1081a2da861d8e051605b692271e0b96
achaney4 <achan014@odu.edu>
2024-07-08 21:45:23 -0400

    made styling edits

--- client/src/features/welcome/pages/Welcome.jsx
+++ client/src/features/welcome/pages/Welcome.jsx
@@ -8,19 +8,19 @@ function Welcome() {
             {/**Navbar */}
             <WelcomeNav />
             <div id="back" className="min-h-screen bg-black">
-                <div className="flex flex-row justify-between">
-                    <div className="flex flex-col" id="textCont">
-                        <div className="flex flex-row justify-start">
-                            <h1 className="font-bold" id="headline">Take Your Fitness To The Next Level!</h1>  
+                <div className="flex flex-row">
+                    <div className="flex flex-col w-full" id="textCont">
+                        <div className="flex flex-row ">
+                            <h1 className="font-bold text-[50px] sm:text-[67px]" id="headline">Take Your Fitness To The Next Level!</h1>  
                         </div>
                 
                         <div className="flex flex-row justify-start ">
-                            <p id="subtitle" className="font-bold">LvlUp offers customized meal plans,<br></br> effective workouts, and more ways to <br></br>level up your fitness!</p>
+                            <p id="subtitle" className="font-bold text-[30px] sm:text-[29px]">LvlUp offers customized meal plans,<br></br> effective workouts, and more ways to <br></br>level up your fitness!</p>
                         </div>
                
                         <div>
                             <button
-                                className=" font-bold rounded-md m-10 px-20 py-2 leading-6 text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+                                className="font-bold rounded-md m-10 px-10 sm:px-20 py-2 leading-6 text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
                                 id="signUp"
                             >
                                 <a href="/register"> Sign Up Now! </a>{/* Sign up button */}
@@ -28,7 +28,7 @@ function Welcome() {
                         </div>
                     </div>
                  
-                    <img id="welcomePic"  src={welcomePic}  />
+                    <img id="welcomePic"  src={welcomePic} className="hidden lg:block"  />
                     
                 </div>
             </div>  
