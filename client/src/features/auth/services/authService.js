34d427296de7b39c2c9871050443acf1d493c509
modusami <128193218+modusami@users.noreply.github.com>
2024-07-09 21:08:32 -0400

    Update authService.js

--- client/src/features/auth/services/authService.js
+++ client/src/features/auth/services/authService.js
@@ -1,26 +1,6 @@
 import { api } from "../../../services/axiosInstance";
 
 const authService = {
-	login: async (user) => {
-		try {
-			const response = await api.post("/auth/login", user);
-			return response.data;
-		} catch (error) {
-			console.error("Login error:", error);
-			throw error;
-		}
-	},
-
-	signup: async (user) => {
-		try {
-			const response = await api.post("/auth/signup", user);
-			return response.data;
-		} catch (error) {
-			console.error("Login error:", error);
-			throw error;
-		}
-	},
-
 	checkAuth: async () => {
 		try {
 			const response = await api.get("/auth/isAuthenticated", {
@@ -31,11 +11,6 @@ const authService = {
 			console.error("Authentication check failed:", error);
 			return false;
 		}
-	},
-
-	logout: async () => {
-		const response = await api.post("auth/logout");
-		return response.data;
 	},
 };
 
