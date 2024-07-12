f3c11820307726679af13e3666fb137a55b82c24
Marcos Sanabria <sanabriam007@gmail.com>
2024-07-12 14:23:46 -0500

    added rest of user info + aligned center

--- client/src/features/home/components/UserCard.jsx
+++ client/src/features/home/components/UserCard.jsx
@@ -10,14 +10,14 @@ export default function UserCard({ userData }) {
   } = userData;
 
   return (
-    <div className="relative flex flex-col mt-6 p-5 bg-dark-grey shadow-md bg-clip-border rounded-xl w-72">
+    <div className="relative flex flex-col items-center mt-10 ml-6 p-5 bg-dark-grey shadow-md bg-clip-border rounded-xl w-72">
       {/* leaderboard number */}
-      <div>
+      <div classsName="text-center">
         <p>#{leaderboardNum}</p>
       </div>
 
       {/* profile pic & username */}
-      <div>
+      <div className="flex flex-col items-center mt-5">
         <img
           class="w-10 h-10 rounded-full"
           src={profilePicture}
@@ -31,11 +31,13 @@ export default function UserCard({ userData }) {
         <p>{fullName}</p>
       </div>
 
-      {/* level and progress bar*/}
-      <div className="mb-4">
+      {/* level and progres bar section */}
+      <div className="w-full mb-4">
+        {/* level */}
         <div className="flex justify-between mb-1">
           <span>Lvl. {level}</span>
         </div>
+        {/* progress bar */}
         <div className="w-full bg-input-grey rounded-full h-2.5">
           <div
             className="h-2.5 rounded-full"
@@ -45,6 +47,13 @@ export default function UserCard({ userData }) {
                 "linear-gradient(90deg, #d00000 0%, #e85d04 50%, #ffba08 100%)",
             }}
           ></div>
+        </div>
+      </div>
+
+      {/* Bio bubble */}
+      <div className="h-auto w-full bg-input-grey flex items-center justify-center">
+        <div className="p-3 text-center">
+          <p>{bio}</p>
         </div>
       </div>
     </div>
